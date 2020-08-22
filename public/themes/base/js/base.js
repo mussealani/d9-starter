1238ebd0a9a145dcda1a79c976349229f96e377e
Mohamad Rashid <mohamadrashid@MBPsomtrMohamad.lan>
2020-08-22 23:23:44 +0200

    Created base theme

---
+++ public/themes/base/js/base.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * base behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.base = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
